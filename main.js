4a62d63fcf57106bc15632cdc01572976283d6c2
Jonatan Arevalo <jonatanare.dev@gmail.com>
2022-08-11 00:35:10 -0500

    ajercicio 1 modulo maquetado

---
+++ main.js
@@ -0,0 +1,10 @@
+let numero1 = 4;
+let numero2 = 4;
+
+if (numero1 > numero2) {
+  console.log(`El numero ${numero1} es mayor que ${numero2}`);
+} else if (numero2 > numero1) {
+  console.log(`El numero ${numero2} es mayor que ${numero1}`);
+} else {
+  console.log("Ambos numero son iguales");
+}
